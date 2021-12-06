bafe2e778069d7375c84ac01eb24da082d7cc931
mgrmanu10 <mgrmanu10@gmail.com>
2021-12-06 19:52:48 +0100

    modificado el app get healtz

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const expressConst = require('express');
 const cors = require('cors');
 const app = expressConst();
+
+const BASE_API_PATH='/api/v1';
 //settings
 app.set('port',3000);
 //middlewares, hace que dos servidores intercambien datos entre ellos
 app.use(cors());
 app.use(expressConst.json());
 //routes
-app.use('/api/notas', require('./routes/notas'));
+app.use('/api/v1/notas', require('./routes/notas'));
 
 //Para saber que el pod de okteto sigue vivo.
-app.get("localhost:3000/api/v1/healthz",(req,res)=>{
+app.get(BASE_API_PATH +"/healthz",(req,res)=>{
     res.sendStatus(200);
 });
 
