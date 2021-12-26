1bcb397a8c1f622e06bc023541399c88b2e5d285
RocVecino <94741402+RocVecino@users.noreply.github.com>
2021-12-26 20:27:46 +0100

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const DB_URL= (process.env.MONGO_URL || 'mongodb+srv://dbUser:ryo5XzXwDIsUCkrT@cluster0.ahojs.mongodb.net/DB_ServicioNotas');
+const DB_URL= (process.env.MONGO_URL || 'mongodb://mongo/test');
 
 const dbConnection = async()=>{
 
