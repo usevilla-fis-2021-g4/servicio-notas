39a1652ab17060feb2a3ba55d4d8131fc7b7d435
Manu <mgrmanu10@gmail.com>
2021-12-30 00:10:11 +0100

    primer test

---
+++ tests/server.test.js
@@ -0,0 +1,12 @@
+
+
+describe("Hello world tests",()=>{
+    
+    it("Should do an stupid test", ()=>{
+        const a=3;
+        const b=5;
+        const sum=a+b;
+
+        expect(sum).toBe(5);
+    });
+});
